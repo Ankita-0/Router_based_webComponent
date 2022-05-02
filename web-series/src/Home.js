d5ae57b134c056d51d44d9d35d57686e2c1beab6
Ankita-0 <ankita.saxena2301@gmail.com>
2022-05-02 13:23:35 +0530

    routing

--- web-series/src/Home.js
+++ web-series/src/Home.js
@@ -7,6 +7,14 @@ export class Home extends LitElement {
         div {
             color:azure;
             font-size:10vw;
+            text-align: center;
+            text-shadow: 3px 3px 5px rgb(0, 255, 200);
+        }
+        .home_page {
+            text-align: center;
+            font-size:3vw;
+            color: blueviolet;
+            text-shadow: 1px 1px 2px black;
         }
         `;
     }
@@ -16,6 +24,10 @@ export class Home extends LitElement {
         <div>
         Web series
         </div>
+        <p class=home_page>
+            <b> Click on form and add the details of your favourite web series <b><br>
+            <b> Click on Overview to view details of all the added webseries <b>
+        </p>
         `;  
     }
 }        
